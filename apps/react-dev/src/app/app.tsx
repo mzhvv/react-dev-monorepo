8b3dc85ac10cf692dbf4040a5631963348c0deaa
mzhvv <maximmezhevov@gmail.com>
2025-09-23 06:05:27 +0300

    feat: migrate to Tailwind CSS v4

--- apps/react-dev/src/app/app.tsx
+++ apps/react-dev/src/app/app.tsx
@@ -1,50 +1,15 @@
-import NxWelcome from './nx-welcome';
+// apps/react-dev/src/app/app.tsx
 
-import { Route, Routes, Link } from 'react-router-dom';
-
-export function App() {
+export const App: React.FC = () => {
   return (
-    <div>
-      <NxWelcome title="@react-dev-monorepo/react-dev" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
+    <div className="sapce-y-5">
+      <header className="h-20 flex flex-col items-center justify-center">
+        <h1 className="text-xl font-semibold">Welcome to react-dev</h1>
+        <p className="text-sm text-neutral-400">
+          @react-dev-monorepo/react-dev
+        </p>
+      </header>
+      <main className="text-center">...</main>
     </div>
   );
-}
-
-export default App;
+};
